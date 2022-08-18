8d922245cdf03c85f4c4e3ef2b25ff75a54034ed
ordovice <dw@trugeek.us>
2022-08-18 08:22:51 -0400

    Missing colons
    Fix missed colons

--- system/switch/configgen/generators/ryujinx/ryujinxMainlineGenerator.py
+++ system/switch/configgen/generators/ryujinx/ryujinxMainlineGenerator.py
@@ -22,13 +22,13 @@ class RyujinxMainlineGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
         #handles chmod so you just need to download Ryujinx.AppImage
-        if os.path.exists("/userdata/system/switch/Ryujinx.AppImage")
+        if os.path.exists("/userdata/system/switch/Ryujinx.AppImage"):
             st = os.stat("/userdata/system/switch/Ryujinx.AppImage")
             os.chmod("/userdata/system/switch/Ryujinx.AppImage", st.st_mode | stat.S_IEXEC)
-        if os.path.exists("/userdata/system/switch/Ryujinx-Avalonia.AppImage")
+        if os.path.exists("/userdata/system/switch/Ryujinx-Avalonia.AppImage"):
             st = os.stat("/userdata/system/switch/Ryujinx-Avalonia.AppImage")
             os.chmod("/userdata/system/switch/Ryujinx-Avalonia.AppImage", st.st_mode | stat.S_IEXEC)  
-                      
+
         if not path.isdir(batoceraFiles.CONF + "/Ryujinx"):
             os.mkdir(batoceraFiles.CONF + "/Ryujinx")
         if not path.isdir(batoceraFiles.CONF + "/Ryujinx/system"):
