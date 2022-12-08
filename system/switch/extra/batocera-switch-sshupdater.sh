73505f86707ffea0aead92e096f9e70b0de81e8f
uureel <116395185+uureel@users.noreply.github.com>
2022-12-08 18:42:11 +0100

    +fix

--- system/switch/extra/batocera-switch-sshupdater.sh
+++ system/switch/extra/batocera-switch-sshupdater.sh
@@ -459,7 +459,7 @@ echo 'ln -s /userdata/system/switch/yuzuEA.AppImage /usr/bin/yuzu 2>/dev/null' >
 echo 'ln -s /userdata/system/switch/extra/yuzuea/yuzu-room /usr/bin/yuzu-room 2>/dev/null' >> $ai
 echo '/userdata/system/switch/extra/yuzuea/yuzu "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" ' >> $ai
 chmod a+x $ai 2>/dev/null
-chmod a+x /userdata/system/switch/extra/yuzu/yuzu-room 2>/dev/null
+chmod a+x /userdata/system/switch/extra/yuzuea/yuzu-room 2>/dev/null
 size_yuzuea=$(($(wc -c $temp/yuzuea/yuzuEA.AppImage | awk '{print $1}')/1048576)) 2>/dev/null
 echo -e "${T}$path_yuzuea ${T}($size_yuzuea( )MB) ${THEME_COLOR_OK}OK" | sed 's/( )//g'
 echo
