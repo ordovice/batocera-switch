e51aed75040882165371802ff3a91b55f23162c6
uureel <116395185+uureel@users.noreply.github.com>
2022-12-24 10:38:48 +0100

    Update batocera-switch-updater.sh

--- system/switch/extra/batocera-switch-updater.sh
+++ system/switch/extra/batocera-switch-updater.sh
@@ -199,12 +199,10 @@ echo 'ff=/userdata/bios/switch/firmware' >> $startup
 echo 'ft=/userdata/bios/switch/_firmware_' >> $startup
 echo 'fr=/userdata/system/configs/Ryujinx/bis/system/Contents/registered' >> $startup
 echo 'fy=/userdata/system/configs/yuzu/nand/system/Contents/registered' >> $startup
-#echo 'rm -rf $ft 2>/dev/null' >> $startup
 echo 'mkdir -p $ff 2>/dev/null' >> $startup
 echo 'mkdir -p $ft 2>/dev/null' >> $startup
 echo 'mkdir -p $fr 2>/dev/null' >> $startup
 echo 'mkdir -p $fy 2>/dev/null' >> $startup
-#echo 'mv $ff $ft 2>/dev/null' >> $startup
 echo "sff=\$(du -s \$ff | awk '{print \$1}')" >> $startup
 echo "sft=\$(du -s \$ft | awk '{print \$1}')" >> $startup
 echo "sfr=\$(du -s \$fr | awk '{print \$1}')" >> $startup
