00ab0ad5d3a1334f204951e8be773b660374ff68
uureel <116395185+uureel@users.noreply.github.com>
2023-06-22 11:23:25 +0200

    v37 fixes for nsz-converter
    v37 fixes for nsz-converter

--- system/switch/extra/batocera-switch-nsz-converter.sh
+++ system/switch/extra/batocera-switch-nsz-converter.sh
@@ -2,81 +2,139 @@
 # batocera-switch nsz-converter 
 #########################################################################################################################
 #
+#-------------------------------------------
 # get rom
-rom="$(cat /tmp/switchromname)"
+#-------------------------------------------
+	rom="$(cat /tmp/switchromname)"
 #
+#-------------------------------------------
 # set rev 
-cp /userdata/system/switch/extra/batocera-switch-rev /usr/bin/rev 2>/dev/null 
-chmod a+x /usr/bin/rev 2>/dev/null 
+#-------------------------------------------
+	cp /userdata/system/switch/extra/batocera-switch-rev /usr/bin/rev 2>/dev/null 
+		chmod a+x /usr/bin/rev 2>/dev/null 
 # 
-##### (b 1) 
+#####  
 	# 
 	if [[ "$(echo "$rom" | rev | cut -c 1-4 | rev)" = ".nsz" ]]; then 
 	#
-	# check nsz converter --------------
-	#-----------------------------------
-	###### (b 2) 
-
+	##### \
+		# ------------------------------------------------------ 
+		# check if nsz converter is available, if not install it  
+		#
 		if [[ "$(which nsz | head -n 1 | grep "not found")" != "" ]] || [[ "$(which nsz | head -n 1)" = "" ]]; then 
 
-			function nsz-install() {
-				echo -e "\033[0;37mPREPARING NSZ CONVERTER ... \033[0;30m \n"
-				python -m ensurepip --default-pip 1>/dev/null 2>/dev/null 
-				python -m pip install --upgrade pip 1>/dev/null 2>/dev/null 
-				python -m pip install --upgrade --force-reinstall pycryptodome 1>/dev/null 2>/dev/null 
-				python -m pip install --upgrade --force-reinstall nsz 1>/dev/null 2>/dev/null 
-				wait
-				sleep 0.1
+			function nsz-install() 
+			{
+				echo -e "╔═════════════════════════════════════════════╗ "
+				echo -e "║ PREPARING NSZ CONVERTER . . .               ║ "
+				echo -e "╚═════════════════════════════════════════════╝ "
+					echo
+					echo
+						python -m ensurepip --default-pip 1>/dev/null 2>/dev/null 
+						python -m pip install --upgrade pip 1>/dev/null 2>/dev/null 
+						python -m pip install --upgrade --force-reinstall pycryptodome 1>/dev/null 2>/dev/null 
+						python -m pip install --upgrade --force-reinstall nsz 1>/dev/null 2>/dev/null 
+						wait
+							sleep 0.1
 			}
 			export -f nsz-install
 
+			# run the nsz installer 
 			cp /usr/bin/xterm /usr/bin/nszinstall 2>/dev/null
-			DISPLAY=:0.0 /usr/bin/nszinstall -fs 8 -fullscreen -fg white -bg black -fa Monospace -en UTF-8 -e bash -c "nsz-install" 2>/dev/null 
-			wait
-			killall -9 nszinstall && rm /usr/bin/nszinstall 2>/dev/null
-			 
+			chmod a+x /usr/bin/nszinstall
+				DISPLAY=:0.0 /usr/bin/nszinstall -fs 8 -fullscreen -fg black -bg gray -fa Monospace -en UTF-8 -e bash -c "nsz-install" 2>/dev/null 
+				wait
+					killall -9 nszinstall && rm /usr/bin/nszinstall 2>/dev/null
+		fi
+		# ------------------------------------------------------ 
+		# /
+		# 
+		##### \\
+			# ==================================================
+			# run nsz converter 
+			#
+			function convert-nsz() 
+			{
+				echo -e "╔═════════════════════════════════════════════╗ "
+				echo -e "║ CONVERTING NSZ TO NSP . . .                 ║ "
+				echo -e "╚═════════════════════════════════════════════╝ "
+					echo
+					echo
 
-		fi # (e 2)
-	######## 
-	# f/convert-------------
-	#-----------------------
-		function convert-nsz() {
-			echo -e "\033[0;37mCONVERTING ROM FROM NSZ TO NSP ... \033[0;30m \n"
-			# get rom 	
-			rom="$(cat /tmp/switchromname)"
+					#-------------------------------------------
+					# get rom from launcher cookie 
+					#-------------------------------------------
+					rom="$(cat /tmp/switchromname)"
 
-			# fill dependencies 
-			cp -r /userdata/system/switch/extra/nsz/curses /usr/lib/python3.10/site-packages/ 2>/dev/null
-			cp -r /userdata/system/switch/extra/nsz/curses /usr/lib/python3.11/site-packages/ 2>/dev/null
-			chmod a+x /userdata/system/switch/extra/nsz/lib-dynload/*.so 2>/dev/null
-			cp -r /userdata/system/switch/extra/nsz/lib-dynload/_curses* /usr/lib/python3.10/lib-dynload/ 2>/dev/null
-			cp -r /userdata/system/switch/extra/nsz/lib-dynload/_curses* /usr/lib/python3.11/lib-dynload/ 2>/dev/null
-			cp /userdata/bios/switch/prod.keys /usr/bin/keys.txt 2>/dev/null
+					#-------------------------------------------
+					# fill dependencies 
+					#-------------------------------------------
+					chmod a+x /userdata/system/switch/extra/nsz/lib-dynload/*.so 2>/dev/null
+						#
+						# for python 3.11
+						if [[ -d "/usr/lib/python3.11" ]]; then 
+							cp -r /userdata/system/switch/extra/nsz/curses /usr/lib/python3.11/site-packages/ 2>/dev/null
+							cp /userdata/system/switch/extra/nsz/lib-dynload/_curses.cpython-310-x86_64-linux-gnu.so /usr/lib/python3.11/lib-dynload/_curses.cpython-311-x86_64-linux-gnu.so
+							cp /userdata/system/switch/extra/nsz/lib-dynload/_curses_panel.cpython-310-x86_64-linux-gnu.so /usr/lib/python3.11/lib-dynload/_curses_panel.cpython-311-x86_64-linux-gnu.so
+							fi
+						#
+						# for python 3.10
+						if [[ -d "/usr/lib/python3.10" ]]; then 
+							cp -r /userdata/system/switch/extra/nsz/curses /usr/lib/python3.10/site-packages/ 2>/dev/null
+							cp /userdata/system/switch/extra/nsz/lib-dynload/_curses.cpython-310-x86_64-linux-gnu.so /usr/lib/python3.10/lib-dynload/_curses.cpython-310-x86_64-linux-gnu.so
+							cp /userdata/system/switch/extra/nsz/lib-dynload/_curses_panel.cpython-310-x86_64-linux-gnu.so /usr/lib/python3.10/lib-dynload/_curses_panel.cpython-310-x86_64-linux-gnu.so
+							fi
 
-			# convert 
-			sleep 0.5 && nsz -D -w -t 4 -P "$rom" 
-			wait
-			echo -e "\n\n\033[0;37mROM CONVERTED TO NSP \033[0;30m \n\n"
-			sleep 0.5 
-			
-					# remove nsz file 
-					rm -rf "$rom" 2>/dev/null 
-					# reload games 
+					#-------------------------------------------
+					# fill user keys 
+					#-------------------------------------------
+					cp /userdata/bios/switch/prod.keys /usr/bin/keys.txt 2>/dev/null
+
+					#-------------------------------------------
+					# run conversion  
+					#-------------------------------------------
+					sleep 0.5 && nsz -D -w -t 4 -i -P "$rom" 
+					wait
+						echo -e "╔═════════════════════════════════════════════╗ "
+						echo -e "║ FINISHED CONVERTING TO NSP                  ║ "
+						echo -e "╚═════════════════════════════════════════════╝ "
+						sleep 0.5 
+					
+					#-------------------------------------------
+					# remove the nsz file 
+					#-------------------------------------------
+					rm -rf "$rom" 2>/dev/null
+
+					#-------------------------------------------
+					# & reload games to remove the nsz entry  
+					#-------------------------------------------
 					curl http://127.0.0.1:1234/reloadgames 
-		} 
-		export -f convert-nsz
 
-	# run in xterm -------------------------------------
-	#---------------------------------------------------
-			DISPLAY=:0.0 xterm -fs 8 -fullscreen -fg white -bg black -fa Monospace -en UTF-8 -e bash -c "clear && convert-nsz && sleep 1 && clear" 2>/dev/null 
-
-			# pass cookie ------------------------------
+			} 
+			export -f convert-nsz
+			#
+			#
 			#-------------------------------------------
-				rom=$(echo "$rom" | sed 's,.nsz,.nsp,g')
-				rm /tmp/switchromname 2>/dev/null
-				echo "$rom" >> /tmp/switchromname 
-	fi # (e 1)
-	   #
-########
-#
-#########################################################################################################################
+			# run in xterm 
+			#-------------------------------------------
+				cp /usr/bin/xterm /usr/bin/nszconvert 2>/dev/null
+				chmod a+x /usr/bin/nszconvert 
+					DISPLAY=:0.0 /usr/bin/nszconvert -fs 8 -fullscreen -fg black -bg gray -fa Monospace -en UTF-8 -e bash -c "clear && convert-nsz && sleep 1 && clear" 2>/dev/null 
+					wait
+				killall -9 nszconvert && rm /usr/bin/nszconvert 2>/dev/null
+
+			#-------------------------------------------
+			# pass the nsp cookie for launcher&emulator 
+			#-------------------------------------------
+				rompath="$(dirname "$rom")"
+				romname="$(basename "$rom" ".nsz")"
+					rom="$rompath/$romname.nsp"
+						rm /tmp/switchromname 2>/dev/null
+							echo "$rom" >> /tmp/switchromname 
+			# // 					
+	######### ==================================================
+	#
+	fi 
+	exit 1
+	# 
+##### (/f1)
