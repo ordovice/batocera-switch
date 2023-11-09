805b4d8dcfb8011c35a2298ec51086840dcc11c2
uureel <116395185+uureel@users.noreply.github.com>
2023-11-09 20:59:36 +0100

    Update batocera-switch-mousemove.sh

--- system/switch/extra/batocera-switch-mousemove.sh
+++ system/switch/extra/batocera-switch-mousemove.sh
@@ -3,13 +3,17 @@
 ##################################################
 
 # get screen resolution 
-  r=$(xrandr | grep " connected" | awk '{print $3}')
-  w=$(echo $r | cut -dx -f1 | cut -d+ -f2)
-  h=$(echo $r | cut -d+ -f1 | cut -dx -f2)
+  r=$(xrandr | grep "+" | awk '{print $1}' | tail -n1)
+  w=$(echo "$r" | cut -d "x" -f1)
+  h=$(echo "$r" | cut -d "x" -f2)
 
 # prepare dependencies 
   ln -sf /userdata/system/switch/extra/batocera-switch-libxdo.so.3 /lib/libxdo.so.3
   ln -sf /userdata/system/switch/extra/batocera-switch-xdotool /usr/bin/xdotool
  
-# move mouse cursor to bottom right corner 
-  xdotool mousemove $w $h
+# move mouse cursor to bottom right corner
+if [[ "$w" =~ ^[1-9][0-9]{2,}$ ]] && [[ "$h" =~ ^[1-9][0-9]{2,}$ ]]; then
+  xdotool mousemove --sync $w $h 2>/dev/null
+else 
+  xdotool mousemove --sync 0 0 2>/dev/null
+fi
